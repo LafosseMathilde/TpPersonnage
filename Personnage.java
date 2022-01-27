ffea1ef5bea5cc7f28619183443a7c8f7b56d6d2
LafosseMathilde <98532717+LafosseMathilde@users.noreply.github.com>
2022-01-27 14:25:20 +0100

    Add files via upload

---
+++ Personnage.java
@@ -0,0 +1,22 @@
+package Personnage;
+public class Personnage {
+//le constructeur
+public Personnage (string name, int life) {
+	this.nom = name;
+	this.vie = life;
+}
+
+//Accesseur du nom du Personnage
+public string LeNom()'
+	return this.nom; }
+	
+	
+//les attributs 
+	private string nom;
+	private int vie; }
+	
+
+
+
+
+
---
+++ Personnage/JeuPersonnage.java
@@ -0,0 +1,10 @@
+import Personnage.*;
+public class JeuPersonnage 
+public static void main (string args[]) {
+	Personnage UneSorciere = new Personnage ("Mathilde",200);
+	system.out.println("nom du Personnage="+UneSorciere.LeNom());
+	}
+	
+	
+	
+	
