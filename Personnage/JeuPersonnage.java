53a28ba75e2c80d160a265f23c6c77b9cdf208e6
LafosseMathilde <98532717+LafosseMathilde@users.noreply.github.com>
2022-02-09 09:13:37 +0100

    Update JeuPersonnage.java

--- Personnage/JeuPersonnage.java
+++ Personnage/JeuPersonnage.java
@@ -1,3 +1,4 @@
+<?php
 import Personnage.*;
 public class JeuPersonnage 
 public static void main (string args[]) {
@@ -5,6 +6,23 @@ public static void main (string args[]) {
 	system.out.println("nom du Personnage="+UneSorciere.LeNom());
 	}
 	
-	
-	
-	
+public class jeuPersonnage{
+    public static void main(String args[]) {
+        Personnage UneSorciere = new Personnage("Mathilde",200);
+       System.out.println("Avant la vie de Fred est " + UnSorcier.LaVie());
+
+        Personnage UnMagicien = new Personnage("Bob",100);
+       System.out.println("Avant la vie de Bob est " + UnMagicien.LaVie());
+
+        System.out.println("Le nom du sorcier = " + UnSorcier.LeNom());
+        System.out.println("Le nom du magicien = " + UnMagicien.LeNom());
+
+       System.out.println("Avant la vie du sorcier est " + UnSorcier.LaVie());
+       UnSorcier.AugmenterVie(10);
+       System.out.println("Apres la vie du sorcier est " + UnSorcier.LaVie());
+
+       UnMagicien.Attaque(UnSorcier,40);
+       System.out.println("Atak La vie du magicien est " + UnMagicien.LaVie());
+       System.out.println("Atak La vie du sorcier est " + UnSorcier.LaVie());
+}
+} 
