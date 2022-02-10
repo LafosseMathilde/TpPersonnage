dac0859d2764885cc1a8ef17ce563657281c4b35
LafosseMathilde <98532717+LafosseMathilde@users.noreply.github.com>
2022-02-10 14:23:51 +0100

    Update JeuPersonnage.java

--- Personnage/JeuPersonnage.java
+++ Personnage/JeuPersonnage.java
@@ -2,11 +2,11 @@ import Personnage.*;
 public class jeuPersonnage{
 public static void main (String args[]){
 Personnage UneGuerrière = new Personnage ("Trabucco", 100, 25);
-Personnage UnRodeur = new Personnage ("LeRusse", 60, 20);
+Personnage Immortel = new Personnage ("LeRusse", 60, 20);
 Personnage UnSorcier = new Personnage ("Quentheinnn", 70, 25);
 Personnage UneVictime = new Personnage ("LaPizza", 15, 10);
 Personnage UnChomeur = new Personnage ("Théo", 20, 10);
 system.out.println();
-system.out.println();
+
 
   
