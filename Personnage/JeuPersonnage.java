1c88bcf227101374772e2a9a69e7c363e3c72fa4
LafosseMathilde <98532717+LafosseMathilde@users.noreply.github.com>
2022-02-10 14:17:03 +0100

    Update JeuPersonnage.java

--- Personnage/JeuPersonnage.java
+++ Personnage/JeuPersonnage.java
@@ -1,38 +1,12 @@
- <?php
 import Personnage.*;
 public class jeuPersonnage{
-    public static void main(String args[]) {
-        Personnage UneGuerrière = new Personnage("Mathilde",100);
-       System.out.println(" " + UneGuerrière.LaVie());
+public static void main (String args[]){
+Personnage UneGuerrière = new Personnage ("Trabucco", 100, 25);
+Personnage UnRodeur = new Personnage ("LeRusse", 60, 20);
+Personnage UnSorcier = new Personnage ("Quentheinnn", 70, 25);
+Personnage UneVictime = new Personnage ("LaPizza", 15, 10);
+Personnage UnChomeur = new Personnage ("Théo", 20, 10);
+system.out.println();
+system.out.println();
 
-        Personnage UnRodeur = new Personnage("Thomas",70);
-       System.out.println("Avant la vie de Thomas est " + UnRodeur.LaVie());
-
-        System.out.println("Le nom de la guerrière = " + UnGuerrière.LeNom());
-        System.out.println("Le nom du rodeur = " + UnRodeur.LeNom());
-
-       System.out.println("Avant la vie de la guerrière est " + UneGuerrière.LaVie());
-       UneGuerrière.AugmenterVie(20);
-       System.out.println("Apres la vie de la guerrière est " + UneGuerrière.LaVie());
-
-       UnRodeur.Attaque(UneGuerrière,40);
-       System.out.println("Attaque La vie du Rodeur est " + UnRodeur.LaVie());
-       System.out.println("Attaque La vie de la Guerrière est " + UneGuerrière.LaVie());
-     
-    public void AugmenterVie(int valeur) {
-        this.vie = this.vie + valeur;
-     }
-     
-    public class Combat {
-     private Personnage UneGuerrière;
-     private Personnage UnRodeur;
- 
-    public Combat(Personnage UneGerrière, Personnage UnRodeur) {
-        this.UneGuerrière = UneGuerrière;
-        this.UnRodeur = UnRodeur;
-    }
- 
-}
-}
-
-
+  
