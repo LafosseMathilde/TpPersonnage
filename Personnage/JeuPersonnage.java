b99d1c1cd96d0190f1d5b8dc975e12a5f0368d57
LafosseMathilde <98532717+LafosseMathilde@users.noreply.github.com>
2022-02-10 09:57:24 +0100

    Update JeuPersonnage.java

--- Personnage/JeuPersonnage.java
+++ Personnage/JeuPersonnage.java
@@ -3,21 +3,21 @@ import Personnage.*;
 public class jeuPersonnage{
     public static void main(String args[]) {
         Personnage UneGuerrière = new Personnage("Mathilde",100);
-       System.out.println("Avant la vie de Fred est " + UneGuerrière.LaVie());
+       System.out.println(" " + UneGuerrière.LaVie());
 
         Personnage UnRodeur = new Personnage("Thomas",70);
-       System.out.println("Avant la vie de Bob est " + UnMagicien.LaVie());
+       System.out.println("Avant la vie de Thomas est " + UnMagicien.LaVie());
 
-        System.out.println("Le nom du sorcier = " + UnGuerrière.LeNom());
-        System.out.println("Le nom du magicien = " + UnRodeur.LeNom());
+        System.out.println("Le nom de la guerrière = " + UnGuerrière.LeNom());
+        System.out.println("Le nom du rodeur = " + UnRodeur.LeNom());
 
-       System.out.println("Avant la vie du sorcier est " + UneGuerrière.LaVie());
+       System.out.println("Avant la vie de la guerrière est " + UneGuerrière.LaVie());
        UneGuerrière.AugmenterVie(20);
-       System.out.println("Apres la vie du sorcier est " + UneGuerrière.LaVie());
+       System.out.println("Apres la vie de la guerrière est " + UneGuerrière.LaVie());
 
        UnRodeur.Attaque(UneGuerrière,40);
-       System.out.println("Atak La vie du magicien est " + UnRodeur.LaVie());
-       System.out.println("Atak La vie du sorcier est " + UneGuerrière.LaVie());
+       System.out.println("Ataque La vie du Rodeur est " + UnRodeur.LaVie());
+       System.out.println("Ataque La vie de la Guerrière est " + UneGuerrière.LaVie());
 }
 }
 
